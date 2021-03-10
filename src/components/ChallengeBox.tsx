6523809a51d301b5b812b0a80582263048e64f34
Fabio Francisco <fabinhofrancisco@gmail.com>
2021-03-10 07:38:08 -0300

    feat: cria Componente challenge box active. Usado quando existem dasafios.

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,15 +1,45 @@
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
+  const hasActiveChallenge = true;
+
   return(
     <div className={styles.challengeBoxContainer}>
-      <div className={styles.challengeNotActive}>
-        <strong>Finalize um ciclo para receber um novo desafio.</strong>
-        <p>
-          <img src="icons/level-up.svg" alt="Level Up"/>
-          Avance de nível completando desafios.
-        </p>
-      </div>
+      { hasActiveChallenge ? ( 
+        <div className={styles.challengeActive}>
+          <head>Ganhe 400xp</head>
+
+          <main>
+            <img src="icons/body.svg" />
+            <strong>Novo desafio</strong>
+            <p>Levante e faça uma caminhada de 3 minutos.</p>
+          </main>
+
+          <footer>
+            <button
+              type='button'
+              className={styles.challengeFailedButton}
+            >Falhei</button>
+            
+            <button 
+              type='button'
+              className={styles.challengeSucceededButton}
+            >
+              Completei
+            </button>
+
+          </footer>
+
+        </div>
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <strong>Finalize um ciclo para receber um novo desafio.</strong>
+          <p>
+            <img src="icons/level-up.svg" alt="Level Up"/>
+            Avance de nível completando desafios.
+          </p>
+        </div>
+      ) }
     </div>
   )
 }
